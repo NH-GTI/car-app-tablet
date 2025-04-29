ede29fd8edd49fb1184e6c4555f1efec27628e02
Nassim HADDAD <nassim.haddad.pro@gmail.com>
2025-04-29 17:30:16 +0200

    feat: category list with static data

---
+++ components/Navigation.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="navigation">
+        <input type="text" placeholder="Search" />
+    </div>
+</template>
+
+<style>
+    .navigation {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        margin: 0 0 5rem 0;
+        background-color: rebeccapurple;
+        height: 5rem;
+    }
+    .navigation input {
+        height: 2rem;
+        width: 20rem;
+        margin-right: 1rem;
+    }
+</style>
